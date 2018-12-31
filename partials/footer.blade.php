1143e6b8b73769d180fbed165a3cbf1e2689a78d
nasri ahmed <ahmednasri1991@gmail.com>
2018-12-31 22:04:32 +0100

    firsrtcommit

---
+++ partials/footer.blade.php
@@ -0,0 +1,25 @@
+<div class="footer">
+    <div class="container">
+        <div class="footer-right">
+            <p>© 2017 All rights reserved | All Solution <a href="www.facebook.com/All.Solution.Tunis" target="_blank"></a></p>
+        </div>
+    </div>
+</div>
+<!--smooth-scrolling-of-move-up-->
+<script type="text/javascript">
+    $(document).ready(function() {
+        /*
+         var defaults = {
+         containerID: 'toTop', // fading element id
+         containerHoverID: 'toTopHover', // fading element hover id
+         scrollSpeed: 1200,
+         easingType: 'linear'
+         };
+         */
+
+        $().UItoTop({ easingType: 'easeOutQuart' });
+
+    });
+</script>
+<a href="#" id="toTop" style="display: block;"> <span id="toTopHover" style="opacity: 1;"> </span></a>
+<!--//smooth-scrolling-of-move-up-->
---
+++ partials/header.blade.php
@@ -0,0 +1,98 @@
+<html>
+<head>
+    <title>Skyline | Home </title>
+    <link href="css/style.css" type="text/css" rel="stylesheet" media="all">
+    <link href="css/bootstrap.css" type="text/css" rel="stylesheet" media="all">
+    <link rel="stylesheet" type="text/css" href="css/default.css" />
+    <link rel="stylesheet" type="text/css" href="css/component.css" />
+    <!--web-font-->
+    <link href='http://fonts.googleapis.com/css?family=Raleway:400,100,200,300,500,600,700,800,900' rel='stylesheet' type='text/css'>
+    <!--//web-font-->
+    <!-- Custom Theme files -->
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+    <meta name="keywords" content="Furnishing Responsive web template, Bootstrap Web Templates, Flat Web Templates, Andriod Compatible web template,
+		Smartphone Compatible web template, free webdesigns for Nokia, Samsung, LG, SonyErricsson, Motorola web design" />
+    <script type="application/x-javascript"> addEventListener("load", function() { setTimeout(hideURLbar, 0); }, false); function hideURLbar(){ window.scrollTo(0,1); } </script>
+    <script src="http://ajax.googleapis.com/ajax/libs/jquery/2.1.1/jquery.min.js"></script><link href="css/bootstrap.css" rel="stylesheet" type="text/css" media="all" />
+    <link href="css/easy-responsive-tabs.css" rel='stylesheet' type='text/css'/>
+    <link rel="stylesheet" type="text/css" href="css/jquery-ui.css">
+    <link rel="stylesheet" href="css/flexslider.css" type="text/css" media="screen" property="" />
+    <script type="text/javascript" src="//maps.googleapis.com/maps/api/js"></script>
+    <!-- font-awesome-icons -->
+    <link href="css/font-awesome.css" rel="stylesheet">
+    <!-- //font-awesome-icons -->
+    <link href="//fonts.googleapis.com/css?family=Josefin+Sans:300,400,600,700" rel="stylesheet">
+    <link href="//fonts.googleapis.com/css?family=Lato:100,300,400,700,900" rel="stylesheet">
+
+    <!-- //Custom Theme files -->
+    <!-- js -->
+    <script src="js/jquery.min.js"></script>
+    <!-- //js -->
+    <!-- start-smoth-scrolling-->
+    <script type="text/javascript" src="js/move-top.js"></script>
+    <script type="text/javascript" src="js/easing.js"></script>
+    <script type="text/javascript" src="js/modernizr.custom.53451.js"></script>
+    <script type="text/javascript">
+        jQuery(document).ready(function($) {
+            $(".scroll").click(function(event){
+                event.preventDefault();
+                $('html,body').animate({scrollTop:$(this.hash).offset().top},1000);
+            });
+        });
+    </script>
+    <script type="application/x-javascript"> addEventListener("load", function() { setTimeout(hideURLbar, 0); }, false);
+        function hideURLbar(){ window.scrollTo(0,1); } </script>
+    <script type="text/javascript" src="js/jquery-1.11.1.min.js"></script>
+    <script type="text/javascript" src="js/bootstrap.js"></script>
+    <!-- stylesheet -->
+    <link href="css/bootstrap.css" rel="stylesheet" type="text/css" media="all" />
+    <!-- menu -->
+    <link type="text/css" rel="stylesheet" href="css/cm-overlay.css" />
+    <!-- //menu -->
+    <link href="css/style.css" rel="stylesheet" type="text/css" media="all" />
+    <!-- //stylesheet -->
+    <!-- online fonts-->
+    <link href="//fonts.googleapis.com/css?family=Tangerine:400,700" rel="stylesheet">
+    <link href="//fonts.googleapis.com/css?family=Roboto:100,100i,300,300i,400,400i,500,500i,700,700i,900,900i" rel="stylesheet">
+    <!-- //online fonts -->
+    <!-- font-awesome-icons -->
+    <link href="css/font-awesome.css" type="text/css" rel="stylesheet">
+
+    <!--//end-smoth-scrolling-->
+    <script src="js/easyResponsiveTabs.js" type="text/javascript"></script>
+    <script type="text/javascript">
+        $(document).ready(function () {
+            $('#horizontalTab').easyResponsiveTabs({
+                type: 'default', //Types: default, vertical, accordion
+                width: 'auto', //auto or any width like 600px
+                fit: true   // 100% fit in a container
+            });
+        });
+    </script>
+
+    <div class="container">
+    <div class="col-md-9 top-nav">
+        <span class="menu"><img src="images/menu.png" alt=""/></span>
+        <ul class="nav1">
+            <li><a href="./" class="active">Accueil</a></li>
+            <li><a href="about.html">Projets</a></li>
+            <li><a href="about.html">Equipe</a></li>
+            <li><a href="portfolio">PORTFOLIO</a></li>
+            <li><a href="contact" >Contact</a></li>
+        </ul>
+        <!-- script-for-menu -->
+        <script>
+            $( "span.menu" ).click(function() {
+                $( "ul.nav1" ).slideToggle( 300, function() {
+                    // Animation complete.
+                });
+            });
+        </script>
+        <!-- /script-for-menu -->
+    </div>
+    <div class="col-md-3 header-logo">
+        <a href="index.html"><img src="images/logo-1.png" alt="logo"/></a>
+    </div>
+    <div class="clearfix"> </div>
+</div>
